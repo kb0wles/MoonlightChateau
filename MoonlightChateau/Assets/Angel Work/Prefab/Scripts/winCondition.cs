c824fff2a51cc9d4df09ad155281016ca4269725
JustSen1 <soyangelabdiel16@gmail.com>
2025-12-05 19:08:39 -0500

    winConditions tested: Tracks wins & gives the player 2 choices (true or false ending)

--- MoonlightChateau/Assets/Angel Work/Prefab/Scripts/winCondition.cs
+++ MoonlightChateau/Assets/Angel Work/Prefab/Scripts/winCondition.cs
@@ -27,37 +27,50 @@ public class winCondition : MonoBehaviour
 
     public void ChooseEndings()
     {
-        if (wonAllGames == true)
-        {
-            // Good ending
-            if (winCounter == allGamesWon)
-            {
-                TrueEnding();
-            }
+        if (!wonAllGames)
+            return;
 
-            // Bad ending
-            if (winCounter == allGamesWon)
-            {
-                FalseEnding();
-            }
-        }
+        SceneManager.LoadScene("EndingChoiceScene"); // Replace with the actual scene name
     }
 
     public void WinCount()
     {
         winCounter++;
+
+        if (winCounter >= allGamesWon)
+        {
+            wonAllGames = true;
+        }
     }
 
     void TrueEnding()
     {
         // Go to the next scene
-        SceneManager.LoadScene(trueEnding);
+        if (wonAllGames)
+        {
+            SceneManager.LoadScene(trueEnding);
+        }
     }
 
     void FalseEnding()
     {
         // Go to the next scene
-        SceneManager.LoadScene(falseEnding);
+        if (wonAllGames)
+        {
+            SceneManager.LoadScene(falseEnding);
+        }
 
+    }
+
+    public void PickTrueEnding()
+    {
+        TrueEnding();
+        Debug.Log("TRUE ENDING triggered");
+    }
+
+    public void PickFalseEnding()
+    {
+        FalseEnding();
+        Debug.Log("FALSE ENDING triggered");
     }
 }
