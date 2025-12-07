8fb44ab7eb15cad6b01054edf7ae9edfd25e08cc
bros4lifemichatavo@gmail.com <bros4lifemichatavo@gmail.com>
2025-12-07 16:12:35 -0500

    Enemy is now able to run away from player
    He need a lot of tweaking but there is a start

--- MoonlightChateau/Assets/MGM Manager/Scripts MGM/Enemy ai.cs
+++ MoonlightChateau/Assets/MGM Manager/Scripts MGM/Enemy ai.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
-
+using UnityEngine.AI;
+using System.Collections.Generic;
 public class Enemyai : MonoBehaviour
 {
+    [SerializeField] Transform player;
+    [SerializeField] NavMeshAgent agent;
+    public Transform[] setpoints;
+
+    [SerializeField] float checkinterval;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,24 @@ public class Enemyai : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Transform furthestpoint = null;
+        float furthestdistance = 0f;
+
+        foreach (Transform point in setpoints)
+        {
+            float DistanceFromPlayer = Vector3.Distance(player.position, point.position);
+
+            if(DistanceFromPlayer > furthestdistance)
+            {
+                furthestpoint = point;
+                furthestdistance = DistanceFromPlayer;
+            }
+        }
+
+        if (furthestpoint != null)
+        {
+            agent.SetDestination(furthestpoint.position);
+        }
+
     }
 }
