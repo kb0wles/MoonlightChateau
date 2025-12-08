8ca51b127b1c6d25427374b747d0a07e53f73b28
kb0wles <kbowles@student.fullsail.edu>
2025-12-08 16:13:21 -0500

    scripting progress

--- MoonlightChateau/Assets/Kat/Button Function.cs
+++ MoonlightChateau/Assets/Kat/Button Function.cs
@@ -1,37 +1,54 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonFunction : MonoBehaviour
 {
+    #region Title UI Buttons
+    public void onQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+
     public void onStart()
     {
-
+       UIManager.instance.stateGameStart();
     }
    
     public void onChapter()
     {
-
+       UIManager.instance.stateChapter();
     }
 
-    public void onSettings()
+    public void onOptions()
     {
-
+        UIManager.instance.stateOptions();
     }
 
     public void onCredits()
     {
-
+        UIManager.instance.stateCredits();
     }
+    #endregion
 
-    public void onQuit()
+    #region Menu UI Buttons
+    public void onBack()
     {
-        #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-        #else
-            Application.Quit();
-        #endif
+        UIManager.instance.closeMenu();
+    }
+    public void onResume()
+    {
+        UIManager.instance.stateUnpause();
+    }
+    public void onMG1()
+    {
+        UIManager.instance.stateMinigame();
+        SceneManager.LoadScene("Minigame 1 - E");
     }
 
-
-
+    #endregion
 }
--- MoonlightChateau/Assets/Kat/UI Manager.cs
+++ MoonlightChateau/Assets/Kat/UI Manager.cs
@@ -5,17 +5,20 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
+    //UI containers
+    //all containers will be active by default
+    //menu ui active but submenus will not 
+    [SerializeField] GameObject gameUI;
+    [SerializeField] GameObject titleUI;
+    [SerializeField] GameObject menuUI;
     //menus
     [SerializeField] GameObject menuActive;
     [SerializeField] GameObject menuChapter;
-    [SerializeField] GameObject menuSettings;
+    [SerializeField] GameObject menuOptions;
     [SerializeField] GameObject menuCredits;
     [SerializeField] GameObject menuPause;
     [SerializeField] GameObject menuNotes;
-    //UI containers
-    [SerializeField] GameObject titleUI;
-    [SerializeField] GameObject gameUI;
-    [SerializeField] GameObject menuUI;
+
 
     public bool isPaused;
     float timeScaleOrig;
@@ -24,12 +27,24 @@ public class UIManager : MonoBehaviour
     {
         instance = this;
         timeScaleOrig = Time.timeScale;
+
+        if (SceneManager.GetActiveScene().name == "UI Test Scene")
+        {
+            gameUI.SetActive(true);
+            titleUI.SetActive(true);
+            menuUI.SetActive(true);
+        }
+        else if (SceneManager.GetActiveScene().name == "Minigame 1 - E" ||
+            SceneManager.GetActiveScene().name == "Minigame 2 - E" ||
+            SceneManager.GetActiveScene().name == "Minigame 1 - E")
+        {
+            stateMinigame();
+        }
     }
     void Start()
     {
         Time.timeScale = 1.0f;
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -61,5 +76,38 @@ public class UIManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         menuActive.SetActive(false);
         menuActive = null;
+    }
+    public void stateGameStart()
+    {
+        titleUI.SetActive(false);
+    }
+    public void stateChapter()
+    {
+
+        menuActive = menuChapter;
+        menuActive.SetActive(true);
+    }
+    public void stateOptions()
+    {
+
+        menuActive = menuOptions;
+        menuActive.SetActive(true);
+    }
+    public void stateCredits()
+    {
+        menuActive = menuCredits;
+        menuActive.SetActive(true);
+    }
+    public void stateMinigame()
+    {
+        gameUI.SetActive(false);
+        titleUI.SetActive(false);
+        menuUI.SetActive(true);
+    }
+
+    public void closeMenu()
+    {
+            menuActive.SetActive(false);
+            menuActive = null;
     }
 }
