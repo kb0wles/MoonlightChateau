571ee6f5abad3c49930d8dfc3f3d8313fe47267e
PunamMagar <pmagar@student.fullsail.edu>
2025-12-19 18:53:45 -0500

    Done with character image expanding on hover

--- MoonlightChateau/Assets/Punam Magar/PM-Scripts/PM-UI/IgnoreAlphaClick.cs
+++ MoonlightChateau/Assets/Punam Magar/PM-Scripts/PM-UI/IgnoreAlphaClick.cs
@@ -4,8 +4,29 @@ using UnityEngine.UI;
 public class IgnoreAlphaClick : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [Range(1, 2.0f)]
+    [SerializeField] float hoverScale;
+
+    RectTransform rectTransform;
+    Vector2 originalScale;
+
     void Start()
     {
+        rectTransform = gameObject.GetComponent<RectTransform>();
+        originalScale = rectTransform.sizeDelta;
+
+        //Ignore clicks on transparent areas of the image
         GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+    }
+
+    public void OnHoverEnter()
+    {
+        rectTransform.sizeDelta = new Vector2(rectTransform.rect.width * hoverScale, rectTransform.rect.height * hoverScale);
+    }
+
+    public void OnHoverExit()
+    {
+        rectTransform.sizeDelta = originalScale;
     }
 }
